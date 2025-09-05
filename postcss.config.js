e147480e77880471020316e7e14fdb5d93b8fd2c
Brian Pedone <brian@uppership.com>
2025-09-05 12:23:39 -0400

    docker file

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
+// postcss.config.js
 export default {
-    plugins: { tailwindcss: {}, autoprefixer: {} },
-  }
+    plugins: {
+      '@tailwindcss/postcss': {},
+      autoprefixer: {}
+    }
+  };
   
